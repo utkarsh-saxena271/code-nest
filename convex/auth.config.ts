fa0277de7784a4cf2fd68b40b9228fd8edcb4d6e
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-11-03 21:21:44 +0530

    secured the issuer domain using the env var

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -7,7 +7,7 @@ export default {
       // or with `process.env.CLERK_JWT_ISSUER_DOMAIN`
       // and configure CLERK_JWT_ISSUER_DOMAIN on the Convex Dashboard
       // See https://docs.convex.dev/auth/clerk#configuring-dev-and-prod-instances
-      domain: "https://quick-walrus-47.clerk.accounts.dev",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN!,
       applicationID: "convex",
     },
   ]
